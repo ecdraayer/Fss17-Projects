22424cfc3106d9adbc6ddeb0a773da81f256dee0
ecdraayer <ecdraayer@Gmail.com>
2017-08-31 14:01:40 -0400

    Version 2

--- Homework1/CSVReader.py
+++ Homework1/CSVReader.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import time
 
 def isBad( n ):
   return (len(n) < NumofAttributes or len(n) > NumofAttributes)
@@ -16,17 +17,42 @@ def is_number( s ):
   except ValueError:
     return False
 
+def incomplete(line, f):
+  try:
+    line = str(line) + str(next(f))
+  except:
+    line = line
+  return line
+
+def mysplit(s, delim=None):
+  return [x for x in s.split(delim) if x]
+
+def checktype( value, i ):
+  if is_number(value):
+    if(headers[i][0] == '$' or headers[i][0] == '>' or headers[i][0] == '<'):
+      return True
+    else:
+      return False
+  else:
+    if(headers[i][0].isalpha()):
+      return True
+    else:
+      return False
+
+start_time = time.time()
 row1 = True
 IgnoreList = []
 
 csvFileName = sys.argv[1]
 NumofAttributes = 0
+headers = []
 Data = []
 
 with open(csvFileName) as f:
-  row = []
-
+  Line_Num = 1
   for line in f:
+    row = []
+
     if row1:
       line = removeJunk(line)
       headers = line.split(',')
@@ -40,35 +66,35 @@ with open(csvFileName) as f:
           row.append(headers[x])
       
       Data.append(row)
-      row = []
       row1 = False
+      Line_Num += 1
     else:
+      if line[-2] == ',':
+        line = incomplete(line, f)
+     
       line = removeJunk(line)
-      if line[-1] == ',':
-        line = str(line) + str(next(f))
-
-      values = line.split(',')
+      values = mysplit(line, ',')
 
       if isBad( values ):
-         print "Bad Number of Values"
+         print "Bad Number of Values on Line " + str(Line_Num)
          continue
 
       for x in range(0, len(values)):
 
         if ( x in IgnoreList ):
           continue
+        
+        if (checktype(values[x], x) == False):
+           print "Bad Data Type on line " + str(Line_Num) + ", col " + str(x)
 
         if ( is_number(values[x]) ):
           row.append(float(values[x])) 
         else:
           row.append(values[x])
       
-      
+      Line_Num += 1
       Data.append(row)
-      row = []
 
-print Data
-
-
+print("--- %s seconds ---" % (time.time() - start_time))
 
 
