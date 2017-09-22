1845564edc7d755d0e9771f7dbdd3e634c8e44b7
admin <admin@admins-MacBook-Pro.local>
2017-09-21 20:03:30 -0400

    Update

---
+++ Homework3/hw3.py
@@ -0,0 +1,23 @@
+import sys
+def unsuperRanges():
+  pass
+
+def superRanges():
+  pass
+def main():
+
+  # Generate Random Numbers
+
+  #for i, value in enumerate(randoms):
+  '''
+  if z < 0.2 then return 0.2 + 2*R.r()/100 end
+  if z < 0.6 then return 0.6 + 2*R.r()/100 end
+  return 0.9 + 2*R.r()/100 end
+  '''
+  f = open('output', 'w')
+  f.write("UnSupervised Ranges:\n")
+  f.write("Supervised Ranges:\n")
+  f.close()
+
+if __name__ == "__main__":
+  main()
